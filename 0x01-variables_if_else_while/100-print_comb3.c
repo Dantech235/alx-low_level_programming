6ef7aad47c660881c2df7b389b2a13818b505fbb
Dantech235 <dezeugo8@gmail.com>
2023-06-16 19:41:42 -0700

    file 100-print_comb3.c updated

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combination of two digits.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int dig_1, dig_2;
+
+	for (dig_1 = 0; dig_2 < 9; dig_1++)
+	{
+		for (dig_2 = dig_1 + 1; dig_2 < 10; dig_2++)
+		{
+			putchar((dig_1 % 10) + '0');
+			putchar((dig_2 % 10) + '0');
+
+			if (dig_1 == 0 , dig_2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
