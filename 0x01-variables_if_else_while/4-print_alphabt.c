c37616fc4a1175e951d4ac37071bde9758a658e4
Dantech235 <dezeugo8@gmail.com>
2023-06-16 19:03:31 -0700

    file 4-print_alphabt.c updated

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,23 +5,24 @@
  *
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
-int lowercase = 'a';
-while (lowercase <= 'z')
-{
-if (lowercase == 'e' || lowercase == 'q')
-{
-lowercase += 1;
-}
-else  
-{
-putchar(lowercase);
-lowercase += 1;
-}
-}
-putchar('\n');
-return (0);
+	int lowercase = 'a';
+
+	while (lowercase <= 'z')
+	{
+		if (lowercase == 'e' || lowercase == 'q')
+		{
+			lowercase += 1;
+		}
+		else
+		{
+			putchar(lowercase);
+			lowercase += 1;
+		}
+	}
+
+	putchar('\n');
+	return (0);
 }
 
