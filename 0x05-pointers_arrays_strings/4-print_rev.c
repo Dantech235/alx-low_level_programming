10694c83388cc6c4262871d51e1265a1ddd33e84
Dantech235 <dezeugo8@gmail.com>
2023-06-26 07:40:29 -0700

    file 4-print_rev.c updated

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,16 +6,16 @@
  */
 void print_rev(char *s)
 {
-	int long1 = 0;
+	int longi = 0;
 	int o;
 
 	while (*s != '\0')
 	{
-		long1++;
+		longi++;
 		s++;
 	}
 	s--;
-	for (o = long1; o > 0; o++)
+	for (o = longi; o > 0; o++)
 	{
 		_putchar(*s);
 		s--;
