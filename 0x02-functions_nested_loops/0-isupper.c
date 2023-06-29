446286d452069acbedfac7795c940f280aa0bc6b
Dantech235 <dezeugo8@gmail.com>
2023-06-29 06:02:31 -0700

    file 11-print_to_98.c updated

---
+++ 0x02-functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * int _isupper - check for uppercase character
+ * @c: The character to be computed
+ * Return: return 1 if c is uppercase and 0 if otherwise
+ */
+
+void int _isupper(int c)
+{
+	char c;
+
+	int uppercase = A;
+	int lowercase = a;
+
+	if (c == A)
+	{
+		printf("%c: %d\n", A, _isupper(c) is 1);
+	}
+	else if (c == a)
+	{
+		printf("%c: %d\n", a, _isupper(c) is 0);
+	}
+	return (0);
+}
